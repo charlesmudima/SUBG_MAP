43301e144c70cc263a248b73e8f0892b4bc0ee95
Mudima, TC, Mr [23969156@sun.ac.za] <23969156@sun.ac.za>
2023-04-24 00:00:42 +0200

    FeatureToJson Scripts

--- FeaturesToJson.py
+++ FeaturesToJson.py
@@ -1,17 +1,18 @@
-# import arcpy
+
+# So what do i want to do??
+# I want a script that converts shapefiles to .js
+# The scripts can be stored inside this folder 
+
 import os 
 import json
 import sys
 import arcpy
 import arcpy.sa
 import arcpy.da
+import shapefile
+import geojson
+from geojson import Feature, FeatureCollection, Point
 
-listPath = arcpy.ListFeatureClasses()
-
-
-print (listPath)
-
-print(sys.path)
 arcpy.env.workspace = "c:/data"
 arcpy.FeaturesToJSON_conversion(os.path.join("outgdb.gdb", "myfeatures"),
                                 "myjsonfeatures.json")
